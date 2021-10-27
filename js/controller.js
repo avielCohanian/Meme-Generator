255b29bddab832a531e8759455868daf9913bbc9
Aviel Cohanian <avial151515@gmail.com>
2021-10-27 15:20:58 +0300

    Add img And Load

--- js/controller.js
+++ js/controller.js
@@ -1,17 +1,181 @@
 'use strict'
-
-
+var gCanvas;
+var gCtx;
 
 function onInit() {
-    renderImg()
+    renderImgs()
+    gCanvas = document.querySelector('canvas');
+    gCtx = gCanvas.getContext('2d');
+
+
+    // addListeners()
+}
+
+function renderCanvas() {
+    let currCanvasImg = getCurrImgs()
+    let imgs = getImgs()
+    let currImg = imgs.find(img => img.id === currCanvasImg.selectedImgId)
+    // SelectImg(currImg.id)
+    openImg(currImg.url)
+    drawText(currCanvasImg.lines.txt)
+    console.log(currCanvasImg);
+    //     gCtx.fillStyle = "#ede5ff"
+    //     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+    //     renderCircle()
+    // gCtx.restore()
+}
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+    // window.addEventListener('resize', () => {
+    //     resizeCanvas()
+    //     renderCanvas()
+    // })
+}
+
+function addMouseListeners() {
+    gCanvas.addEventListener('mousemove', onMove)
+    gCanvas.addEventListener('mousedown', onDown)
+    gCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gCanvas.addEventListener('touchmove', onMove)
+    gCanvas.addEventListener('touchstart', onDown)
+    gCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+    const pos = getEvPos(ev)
+    if (!isCircleClicked(pos)) return
+    setCircleDrag(true)
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+}
+
+function onMove(ev) {
+    const circle = getCircle();
+    if (circle.isDrag) {
+        const pos = getEvPos(ev)
+        const dx = pos.x - gStartPos.x
+        const dy = pos.y - gStartPos.y
+        gStartPos = pos
+        moveCircle(dx, dy)
+        renderCanvas()
+    }
+}
+
+function onUp() {
+    setCircleDrag(false)
+    document.body.style.cursor = 'grab'
 }
 
 
 
-function renderImg() {
-    let imgs = getimgs()
+function renderImgs() {
+    let imgs = getImgs()
+    let strHtml = ''
     imgs.forEach(img => {
-// img.url
+        strHtml += ` <img onclick="SelectImg(${img.id})" src=${img.url}>`
     })
+    document.querySelector('.images').innerHTML = strHtml
+}
 
+function SelectImg(elId) {
+    let imgs = getImgs()
+    let currImg = imgs.find(img => img.id === elId)
+    document.body.classList.add('do-meme')
+    openImg(currImg.url)
+    setCurrImgId(elId)
+}
+
+function openImg(url){
+    var img = new Image()
+    img.src = url
+    // img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    // }
+}
+
+function onDrawText(txt) {
+    // gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+    setCurrImgTxt(txt)
+    renderCanvas()
+}
+
+function onTxtColorChange(txtColor) {
+    setTxtColor(txtColor)
+}
+function onBackColorChange(txtColor) {
+    setBackgroundColor(txtColor)
+}
+function increaseFont() {
+    setIncreaseFont()
+}
+function decreaseFont() {
+    setDecreaseFont()
+}
+function centerTextAlignment() {
+    setCenterTextAlignment()
+}
+function alignToRight() {
+    setAlignToRight()
+}
+function alignToLeft() {
+    setAlignToLeft()
+}
+function addImg(img) {
+    addNewImg(img)
+}
+
+
+
+
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+    document.querySelector('canvas').innerHTML = ''
+    var reader = new FileReader()
+
+    reader.onload = function (event) {
+        var img = new Image()
+        img.onload = onImageReady.bind(null, img)
+        img.src = event.target.result
+        renderImg.bind(null, img)
+        addImg(img)
+    }
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+
+
+
+function renderImg(img) {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+    console.log(img);
+}
+
+
+function downloadImg(elLink) {
+    var imgContent = gCanvas.toDataURL('image/jpg')
+    elLink.href = imgContent
+}
+
+function uploadImg() {
+    const imgDataUrl = gCanvas.toDataURL("image/jpeg");
+
+    // A function to be called if request succeeds
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.user-msg').innerText = `Your photo is available here: ${uploadedImgUrl}`
+
+        document.querySelector('.share-container').innerHTML = `
+        <a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+    }
+    doUploadImg(imgDataUrl, onSuccess);
 }
--- js/service/meme-service.js
+++ js/service/meme-service.js
@@ -1,26 +1,26 @@
 'use strict';
-
+var gId = 19;
 
 // var gKeywords = {'happy': 12,'funny puk': 1}
 var gImgs = [
-    { id: 1, url: `<img src=meme-imgs/1.jpg>`, keywords: ['happy'] },
-    { id: 2, url: `<img src=meme-imgs/2.jpg>`, keywords: ['happy'] },
-    { id: 3, url: `<img src=meme-imgs/3.jpg>`, keywords: ['happy'] },
-    { id: 4, url: `<img src=meme-imgs/4.jpg>`, keywords: ['happy'] },
-    { id: 5, url: `<img src=meme-imgs/5.jpg>`, keywords: ['happy'] },
-    { id: 6, url: `<img src=meme-imgs/6.jpg>`, keywords: ['happy'] },
-    { id: 7, url: `<img src=meme-imgs/7.jpg>`, keywords: ['happy'] },
-    { id: 8, url: `<img src=meme-imgs/8.jpg>`, keywords: ['happy'] },
-    { id: 9, url: `<img src=meme-imgs/9.jpg>`, keywords: ['happy'] },
-    { id: 10, url: `<img src=meme-imgs/10.jpg>`, keywords: ['happy'] },
-    { id: 11, url: `<img src=meme-imgs/11.jpg>`, keywords: ['happy'] },
-    { id: 12, url: `<img src=meme-imgs/12.jpg>`, keywords: ['happy'] },
-    { id: 13, url: `<img src=meme-imgs/13.jpg>`, keywords: ['happy'] },
-    { id: 14, url: `<img src=meme-imgs/14.jpg>`, keywords: ['happy'] },
-    { id: 15, url: `<img src=meme-imgs/15.jpg>`, keywords: ['happy'] },
-    { id: 16, url: `<img src=meme-imgs/16.jpg>`, keywords: ['happy'] },
-    { id: 17, url: `<img src=meme-imgs/17.jpg>`, keywords: ['happy'] },
-    { id: 18, url: `<img src=meme-imgs/18.jpg>`, keywords: ['happy'] },
+    { id: 1, url: `meme-imgs/1.jpg`, keywords: ['happy'] },
+    { id: 2, url: `meme-imgs/2.jpg`, keywords: ['happy'] },
+    { id: 3, url: `meme-imgs/3.jpg`, keywords: ['happy'] },
+    { id: 4, url: `meme-imgs/4.jpg`, keywords: ['happy'] },
+    { id: 5, url: `meme-imgs/5.jpg`, keywords: ['happy'] },
+    { id: 6, url: `meme-imgs/6.jpg`, keywords: ['happy'] },
+    { id: 7, url: `meme-imgs/7.jpg`, keywords: ['happy'] },
+    { id: 8, url: `meme-imgs/8.jpg`, keywords: ['happy'] },
+    { id: 9, url: `meme-imgs/9.jpg`, keywords: ['happy'] },
+    { id: 10, url: `meme-imgs/10.jpg`, keywords: ['happy'] },
+    { id: 11, url: `meme-imgs/11.jpg`, keywords: ['happy'] },
+    { id: 12, url: `meme-imgs/12.jpg`, keywords: ['happy'] },
+    { id: 13, url: `meme-imgs/13.jpg`, keywords: ['happy'] },
+    { id: 14, url: `meme-imgs/14.jpg`, keywords: ['happy'] },
+    { id: 15, url: `meme-imgs/15.jpg`, keywords: ['happy'] },
+    { id: 16, url: `meme-imgs/16.jpg`, keywords: ['happy'] },
+    { id: 17, url: `meme-imgs/17.jpg`, keywords: ['happy'] },
+    { id: 18, url: `meme-imgs/18.jpg`, keywords: ['happy'] },
 ];
 var gMeme = {
     selectedImgId: 5,
@@ -34,8 +34,94 @@ var gMeme = {
         }
     ]
 }
+var gCurrMeme = {
+    selectedImgId: 0,
+    lines: {
+        txt: '',
+        size: 20,
+        align: 'center-text-alignmentleft',
+        colorTxt: 'black',
+        colorBackground: 'white'
+    }
+}
 
-function getimgs(){
+function getImgs() {
     return gImgs
 }
-// document.querySelector('div').innerHTML=  gImgs[5].url
+function getCurrImgs() {
+    return gCurrMeme
+}
+
+function setCurrImgId(elId) {
+    gCurrMeme.selectedImgId = elId
+}
+function setCurrImgTxt(txt) {
+    gCurrMeme.lines.txt = txt
+    drawText(txt)
+    // console.log(txt);
+}
+function setTxtColor(color) {
+    gCurrMeme.lines.colorTxt = color
+}
+function setBackgroundColor(color) {
+    gCurrMeme.lines.colorBackground = color
+}
+function setIncreaseFont() {
+    gCurrMeme.lines.size += 1
+}
+function setDecreaseFont() {
+    gCurrMeme.lines.size -= 1
+}
+function setCenterTextAlignment() {
+    gCurrMeme.lines.align = 'center-text-alignment'
+}
+function setAlignToRight() {
+    gCurrMeme.lines.align = 'align-to-right'
+}
+function setAlignToLeft() {
+    gCurrMeme.lines.align = 'align-to-left'
+}
+function addNewImg(img) {
+    gImgs.push( { id: gId++, url: img, keywords: ['happy'] })
+}
+
+
+
+
+
+function drawText(txt) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'brown';
+    gCtx.fillStyle = 'black';
+    gCtx.font = '40px Arial';
+    gCtx.fillText(txt, 10, 50);
+    gCtx.strokeText(txt, 10, 50);
+}
+
+
+
+
+
+
+
+
+
+
+function doUploadImg(imgDataUrl, onSuccess) {
+
+    const formData = new FormData();
+    formData.append('img', imgDataUrl)
+
+    fetch('//ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.text())
+        .then((url) => {
+            console.log('Got back live url:', url);
+            onSuccess(url)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+}
